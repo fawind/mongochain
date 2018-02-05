05acd01e506735c23cbda8d175708a108bd2b307
Fabian Paul <fabian.paul@student.hpi.de>
2018-02-05 16:04:28 +0100

    Capture connecting member ip addresses

--- benchmarks/cluster-setup/stateserver.py
+++ benchmarks/cluster-setup/stateserver.py
@@ -1,17 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-COUNTER = 0
 COMMUNITIES = 2
 PRIMARIES = [True for _ in range(COMMUNITIES)]
+counter = 0
+members = set()
 
 
 @app.route("/getState")
 def get_state():
-    global COUNTER
-    COUNTER += 1
+    global counter
+    global members
+    members.add(request.remote_addr)
+    counter += 1
     return_template = '{}, {}'
-    community = COUNTER % COMMUNITIES
+    community = counter % COMMUNITIES
     primary = PRIMARIES[community]
     PRIMARIES[community] = False
     return return_template.format(community, primary)
+
+
+@app.route("/getMembers")
+def get_members():
+    return jsonify(list(members)), 200
